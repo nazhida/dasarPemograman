eb0a53cab1014ab80d9d018b3b9d3dbac8f7a5be
nazhida <136064036+nazhida@users.noreply.github.com>
2024-10-28 19:13:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+function btn() {
+    const content = document.getElementById('content');
+    const steps = document.querySelectorAll('.step');
+    steps.forEach(s => s.style.display = 'none');
+        content.style.display = 'block';
+}
+function btnc() {
+    const content = document.getElementById('contentc');
+    content.innerHTML = '<h3>Ini PEngeliatan</h3>';
+}
